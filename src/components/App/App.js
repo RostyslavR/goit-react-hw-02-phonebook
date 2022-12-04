1ef2410a5f1bbd2979b6a8ce203a7ed08a75402c
RostyslavR <rrmail@ukr.net>
2022-12-04 17:54:06 +0200

    upd

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,59 @@
+import contacts from 'data/contacts.json';
+import { nanoid } from 'nanoid';
 import { Component } from 'react';
 import { Container } from 'components/App/App.styled';
-import { Section } from 'components/Section/Section';
-import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
-import { Statistics } from 'components/Statistics/Statistics';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { List } from 'components/List/List';
 
 export class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    contacts: [...contacts],
+    filter: '',
   };
 
-  handleLeaveFeedback = name => {
+  handleAdd = ({ name, phone }, { resetForm }) => {
+    const contactId = nanoid();
+
+    const existsContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (existsContact) {
+      alert(`${existsContact.name} this contact already exists`);
+      return;
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, { id: contactId, name, phone }],
+      }));
+    }
+
+    resetForm();
+  };
+
+  handleFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value.toLowerCase() });
+  };
+
+  handleDelete = currentContact => {
     this.setState(prevState => ({
-      [name]: prevState[name] + 1,
+      contacts: prevState.contacts.filter(
+        contact => contact.id !== currentContact
+      ),
     }));
   };
 
   render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter)
+    );
+
     return (
       <Container>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleLeaveFeedback}
-          />
-        </Section>
-        <Section title={'Statistics'}>
-          <Statistics feedbacks={this.state} />
-        </Section>
+        <h1>Phone book</h1>
+        <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
+        <ContactForm handleAdd={this.handleAdd} />
+        <List contacts={filteredContacts} handleDelete={this.handleDelete} />
       </Container>
     );
   }
